2f43024dcdbdba6589572abb9599e53c699af5a6
hessianliu <hessianliu@tencent.com>
2021-10-26 17:28:04 +0800

    format

--- httpserver/simple_http_server.cpp
+++ httpserver/simple_http_server.cpp
@@ -1,30 +1,27 @@
-#include <utility>
 #include <string>
-
+#include <utility>
 
 #include "httpserver/simple_http_server.h"
 
-void SimpleHttpServer::Init(const std::string &port)
-{
-	m_port_ = port;
-	s_server_option.enable_directory_listing = "yes";
-	s_server_option.document_root = s_web_dir.c_str();
+void SimpleHttpServer::Init(const std::string &port) {
+  m_port_ = port;
+  s_server_option.enable_directory_listing = "yes";
+  s_server_option.document_root = s_web_dir.c_str();
 }
 
-bool SimpleHttpServer::Start()
-{
-	mg_mgr_init(&m_mgr_, NULL);
-	mg_connection *connection = mg_bind(&m_mgr_, m_port_.c_str(), SimpleHttpServer::OnHttpEvent);
-	if (connection == NULL)
-		return false;
-	mg_set_protocol_http_websocket(connection);
-	printf("starting http server at port: %s\n", m_port_.c_str());
-	loop_thread_.reset(new std::thread(&SimpleHttpServer::Loop,this));
-	return true;
+bool SimpleHttpServer::Start() {
+  mg_mgr_init(&m_mgr_, NULL);
+  mg_connection *connection =
+      mg_bind(&m_mgr_, m_port_.c_str(), SimpleHttpServer::OnHttpEvent);
+  if (connection == NULL) return false;
+  mg_set_protocol_http_websocket(connection);
+  printf("starting http server at port: %s\n", m_port_.c_str());
+  loop_thread_.reset(new std::thread(&SimpleHttpServer::Loop, this));
+  return true;
 }
 
-void SimpleHttpServer::Stop(){
-  if(terminate_){
+void SimpleHttpServer::Stop() {
+  if (terminate_) {
     return;
   }
   terminate_ = true;
@@ -33,76 +30,72 @@ void SimpleHttpServer::Stop(){
   Close();
 }
 
-void SimpleHttpServer::OnHttpEvent(mg_connection *connection, int event_type, void *event_data)
-{
-	switch(event_type){
-	  case MG_EV_HTTP_REQUEST:
-    {
+void SimpleHttpServer::OnHttpEvent(mg_connection *connection, int event_type,
+                                   void *event_data) {
+  switch (event_type) {
+    case MG_EV_HTTP_REQUEST: {
       http_message *http_req = (http_message *)event_data;
       HandleHttpEvent(connection, http_req);
       break;
     }
     default:
       break;
-	}
-}
-
-void SimpleHttpServer::AddHandler(const std::string &url, ReqHandler req_handler)
-{
-	if (s_handler_map.find(url) != s_handler_map.end())
-		return;
-
-	s_handler_map.insert(std::make_pair(url, req_handler));
-}
-
-void SimpleHttpServer::RemoveHandler(const std::string &url)
-{
-	auto it = s_handler_map.find(url);
-	if (it != s_handler_map.end())
-		s_handler_map.erase(it);
-}
-
-void SimpleHttpServer::SendHttpRsp(mg_connection *connection, const std::string& rsp)
-{
-	mg_printf(connection, "%s", "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\n\r\n");
-	mg_printf_http_chunk(connection, "{ \"result\": %s }", rsp.c_str());
-	mg_send_http_chunk(connection, "", 0);
-}
-
-void SimpleHttpServer::HandleHttpEvent(mg_connection *connection, http_message *http_req)
-{
-	std::string req_str = std::string(http_req->message.p, http_req->message.len);
-	printf("got request: %s\n", req_str.c_str());
-	std::string url = std::string(http_req->uri.p, http_req->uri.len);
-	std::string body = std::string(http_req->body.p, http_req->body.len);
-	auto it = s_handler_map.find(url);
-	if (it != s_handler_map.end())
-	{
-		ReqHandler handle_func = it->second;
-		handle_func(url, body, connection, &SimpleHttpServer::SendHttpRsp);
-	} else if(url == "/"){
-    mg_serve_http(connection, http_req, s_server_option);
-	} else {
-    SendError(connection, url,  501, " Not Implemented");
-	}
-}
-
-void SimpleHttpServer::SendError(mg_connection *connection, const std::string& uri, int err_code, const std::string& reply){
-  mg_printf(
-      connection,
-      "HTTP/1.1 %d %s\r\n"
-      "Content-Length: 0\r\n\r\n",
-      err_code, reply.c_str());
-}
-
-void SimpleHttpServer::Loop(){
-  while(!terminate_){
-      mg_mgr_poll(&m_mgr_, 300);
   }
 }
 
-bool SimpleHttpServer::Close()
-{
-	mg_mgr_free(&m_mgr_);
-	return true;
+void SimpleHttpServer::AddHandler(const std::string &url,
+                                  ReqHandler req_handler) {
+  if (s_handler_map.find(url) != s_handler_map.end()) return;
+
+  s_handler_map.insert(std::make_pair(url, req_handler));
+}
+
+void SimpleHttpServer::RemoveHandler(const std::string &url) {
+  auto it = s_handler_map.find(url);
+  if (it != s_handler_map.end()) s_handler_map.erase(it);
+}
+
+void SimpleHttpServer::SendHttpRsp(mg_connection *connection,
+                                   const std::string &rsp) {
+  mg_printf(connection, "%s",
+            "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\n\r\n");
+  mg_printf_http_chunk(connection, "{ \"result\": %s }", rsp.c_str());
+  mg_send_http_chunk(connection, "", 0);
+}
+
+void SimpleHttpServer::HandleHttpEvent(mg_connection *connection,
+                                       http_message *http_req) {
+  std::string req_str = std::string(http_req->message.p, http_req->message.len);
+  printf("got request: %s\n", req_str.c_str());
+  std::string url = std::string(http_req->uri.p, http_req->uri.len);
+  std::string body = std::string(http_req->body.p, http_req->body.len);
+  auto it = s_handler_map.find(url);
+  if (it != s_handler_map.end()) {
+    ReqHandler handle_func = it->second;
+    handle_func(url, body, connection, &SimpleHttpServer::SendHttpRsp);
+  } else if (url == "/") {
+    mg_serve_http(connection, http_req, s_server_option);
+  } else {
+    SendError(connection, url, 501, " Not Implemented");
+  }
+}
+
+void SimpleHttpServer::SendError(mg_connection *connection,
+                                 const std::string &uri, int err_code,
+                                 const std::string &reply) {
+  mg_printf(connection,
+            "HTTP/1.1 %d %s\r\n"
+            "Content-Length: 0\r\n\r\n",
+            err_code, reply.c_str());
+}
+
+void SimpleHttpServer::Loop() {
+  while (!terminate_) {
+    mg_mgr_poll(&m_mgr_, 300);
+  }
+}
+
+bool SimpleHttpServer::Close() {
+  mg_mgr_free(&m_mgr_);
+  return true;
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <memory>
 #include <map>
+#include <memory>
 #include <vector>
 #include "httpserver/simple_http_server.h"
 #include "utils/base_string.h"
@@ -9,19 +9,17 @@ mg_serve_http_opts SimpleHttpServer::s_server_option;
 std::string SimpleHttpServer::s_web_dir = "./web";
 std::unordered_map<std::string, ReqHandler> SimpleHttpServer::s_handler_map;
 
-bool handle_fun1(const std::string& url, const std::string& body, mg_connection *c, OnRspCallback rsp_callback)
-{
-
-	std::cout << "handle fun1" << std::endl;
-	std::cout << "url: " << url << std::endl;
-	std::cout << "body: " << body << std::endl;
-	rsp_callback(c, "rsp1");
-	return true;
+bool handle_fun1(const std::string& url, const std::string& body,
+                 mg_connection* c, OnRspCallback rsp_callback) {
+  std::cout << "handle fun1" << std::endl;
+  std::cout << "url: " << url << std::endl;
+  std::cout << "body: " << body << std::endl;
+  rsp_callback(c, "rsp1");
+  return true;
 }
 
-bool hello(const std::string& url, const std::string& body, mg_connection *c, OnRspCallback rsp_callback)
-{
-
+bool hello(const std::string& url, const std::string& body, mg_connection* c,
+           OnRspCallback rsp_callback) {
   std::cout << "handle fun1" << std::endl;
   std::cout << "url: " << url << std::endl;
   std::cout << "body: " << body << std::endl;
@@ -29,9 +27,8 @@ bool hello(const std::string& url, const std::string& body, mg_connection *c, On
   return true;
 }
 
-bool sum(const std::string& url, const std::string& body, mg_connection *c, OnRspCallback rsp_callback)
-{
-
+bool sum(const std::string& url, const std::string& body, mg_connection* c,
+         OnRspCallback rsp_callback) {
   std::cout << "handle fun1" << std::endl;
   std::cout << "url: " << url << std::endl;
   std::cout << "body: " << body << std::endl;
@@ -39,12 +36,12 @@ bool sum(const std::string& url, const std::string& body, mg_connection *c, OnRs
   std::vector<std::string> items;
   BaseString::Split(raw_body, items, "&");
   std::map<std::string, std::string> params;
-  for(const auto& e: items){
+  for (const auto& e : items) {
     std::vector<std::string> key_val;
-    BaseString::Split(e,key_val, "=");
+    BaseString::Split(e, key_val, "=");
     std::string key = key_val[0];
     std::string val = "0";
-    if(key_val.size() == 2){
+    if (key_val.size() == 2) {
       val = key_val[1];
     }
     params[key] = val;
@@ -56,28 +53,27 @@ bool sum(const std::string& url, const std::string& body, mg_connection *c, OnRs
   return true;
 }
 
-bool handle_fun2(const std::string& url, const std::string& body, mg_connection *c, OnRspCallback rsp_callback)
-{
-	std::cout << "handle fun2" << std::endl;
-	std::cout << "url: " << url << std::endl;
-	std::cout << "body: " << body << std::endl;
-	rsp_callback(c, "rsp2");
-	return true;
+bool handle_fun2(const std::string& url, const std::string& body,
+                 mg_connection* c, OnRspCallback rsp_callback) {
+  std::cout << "handle fun2" << std::endl;
+  std::cout << "url: " << url << std::endl;
+  std::cout << "body: " << body << std::endl;
+  rsp_callback(c, "rsp2");
+  return true;
 }
 
-int main(int argc, char *argv[]) 
-{
-	std::string port = "7999";
-	auto http_server = std::shared_ptr<SimpleHttpServer>(new SimpleHttpServer);
-	http_server->Init(port);
-	http_server->AddHandler("/api/hello", hello);
+int main(int argc, char* argv[]) {
+  std::string port = "7999";
+  auto http_server = std::shared_ptr<SimpleHttpServer>(new SimpleHttpServer);
+  http_server->Init(port);
+  http_server->AddHandler("/api/hello", hello);
   http_server->AddHandler("/api/sum", sum);
   http_server->AddHandler("/api/fun1", handle_fun1);
-	http_server->AddHandler("/api/fun2", handle_fun2);
-	http_server->Start();
+  http_server->AddHandler("/api/fun2", handle_fun2);
+  http_server->Start();
   std::cout << "server started, exit";
-	sleep(60);
+  sleep(60);
   http_server->Stop();
   std::cout << "server closed, exit";
-	return 0;
+  return 0;
 }
